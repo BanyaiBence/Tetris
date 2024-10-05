8d4027a237f0c29f9cfec819fa7e566e045b802b
BanyaiBence <bencebn@gmail.com>
2024-10-05 13:38:03 +0200

    Added collisions and rotation

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Game:
               [[1, 1, 1], [1, 0, 0]],
               [[1, 1, 1], [0, 0, 1]]
               ]
+    FALL_EVENT = pg.USEREVENT
+    LEFT_EVENT = pg.USEREVENT + 1
+    RIGHT_EVENT = pg.USEREVENT + 2
+    DOWN_EVENT = pg.USEREVENT + 3
     def __init__(self):
         pg.init()
         self.window = pg.display.set_mode((Game.WIDTH, Game.HEIGHT))
@@ -22,120 +26,136 @@ class Game:
         self.clock = pg.time.Clock()
 
         self.matrix = [[0 for i in range(15)] for j in range(24)]
-        self.shape = self.new_shape()
-        self.shape_color = Colors.random_color()
-        self.shape_pos = [0, 5]
-
+        self.reset_shape()
 
     def new_shape(self):
         shape = choice(self.SHAPES)
         return shape
     
     
-    # def draw_shape(self):
+    def draw_shape(self):
+        S = Game.SQUARE_SIZE
+        P = self.shape_pos
+        for i, row in enumerate(self.shape):
+            for j, col in enumerate(row):
+                if col == 0:
+                    continue
+                pg.draw.rect(self.window, self.shape_color,
+                    ((P[1]+j)*S, (P[0]+i)*S, S, S))   
 
     def draw(self):
-        self.window.fill(colors.BLACK)
-
+        self.window.fill(Colors.BLACK)
+        S = Game.SQUARE_SIZE
         for i, row in enumerate(self.matrix):
             for j, col in enumerate(row):
                 if col == 0:
                     continue
                 pg.draw.rect(self.window, col,
-                    (
-                    j*Game.SQUARE_SIZE,
-                    i*Game.SQUARE_SIZE,
-                    Game.SQUARE_SIZE, 
-                    Game.SQUARE_SIZE
-                    ))
+                    (j*S, i*S, S, S))
 
-        for i , row in enumerate(self.shape.matrix):
-            for j, col in enumerate(row):
-                if col == 0:
-                    continue
-                pg.draw.rect(self.window, self.shape.color,
-                    (
-                    (self.shape_pos[1]+j)*Game.SQUARE_SIZE,
-                    (self.shape_pos[0]+i)*Game.SQUARE_SIZE,
-                    Game.SQUARE_SIZE, 
-                    Game.SQUARE_SIZE
-                    )
-                    )
+        self.draw_shape()
         pg.display.flip()
 
+    def schedule_move(self, direction):
+        P = self.shape_pos
+        D = direction
+        self.next_pos = [P[0] + D[0], P[1] + D[1]]
+
+    def move(self):
+        self.shape_pos = self.next_pos
+
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                quit()
+            if event.type in [Game.FALL_EVENT, Game.DOWN_EVENT]:
+                self.schedule_move([1, 0])
+            if event.type == Game.LEFT_EVENT:
+                self.schedule_move([0, -1])
+            if event.type == Game.RIGHT_EVENT:
+                self.schedule_move([0, 1])
+
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_a:
+                    pg.time.set_timer(Game.LEFT_EVENT, 100)
+                if event.key == pg.K_d:
+                    pg.time.set_timer(Game.RIGHT_EVENT, 100)
+                if event.key == pg.K_s:
+                    pg.time.set_timer(Game.DOWN_EVENT, 100)
+                if event.key == pg.K_w:
+                    self.rotate_shape()
+
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_a:
+                    pg.time.set_timer(Game.LEFT_EVENT, 100000)
+                if event.key == pg.K_d:
+                    pg.time.set_timer(Game.RIGHT_EVENT, 100000)
+                if event.key == pg.K_s:
+                    pg.time.set_timer(Game.DOWN_EVENT, 100000)
+
     def run(self):
-        pg.time.set_timer(pg.USEREVENT, 800)
-        pg.time.set_timer(pg.USEREVENT+1, 80)
+        pg.time.set_timer(Game.FALL_EVENT, 500)
         while True:
-            self.clock.tick(120)
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    quit()
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_LEFT:
-                        self.shape_dir = [0, -1]
-                    if event.key == pg.K_RIGHT:
-                        self.shape_dir = [0, 1]
-                    if event.key == pg.K_DOWN:
-                        pg.time.set_timer(pg.USEREVENT, 50)
-                
-                if event.type == pg.KEYUP:
-                    if event.key == pg.K_DOWN:
-                        pg.time.set_timer(pg.USEREVENT, 800)
-                    if event.key == pg.K_LEFT or event.key == pg.K_RIGHT:
-                        self.shape_dir = [0, 0]
-
-
-                if event.type == pg.USEREVENT:
-                    self.fall_shape()
-                if event.type == pg.USEREVENT+1:
-                    if self.shape_dir[0] == 1:
-                        self.fall_shape()
-                    side_dir = [0, self.shape_dir[1]]
-                    self.move_shape(side_dir)
+            self.clock.tick(60)
+            self.events()
+            if not self.collision(self.shape):
+                self.move()
+            self.clear_rows()
             self.draw()
 
-    def set_shape(self):
-        self.shape_dir = [0, 0]
-        for ii in range(4):
-            for jj in range(4):
-                if self.shape.matrix[ii][jj] == 0:
-                    continue
-                self.matrix[self.shape_pos[0]+ii][self.shape_pos[1]+jj] = self.shape.color
+    def in_bounds(self, pos):
+        x = pos[1]
+        y = pos[0]
+        if x < 0 or x >= 15:
+            return False
+        if y < 0 or y >= 24:
+            return False
+        return True
 
-    def fall_shape(self):
-        dir = [1, 0]
-        new_pos = [self.shape_pos[0] + dir[0], self.shape_pos[1] + dir[1]]
-        if new_pos[1] < 0:
-            return
-        right = self.shape.right
-        if new_pos[1] + right > 15:
-            return
-        
-        if new_pos[0] + 4 > 24:
-            self.set_shape()
-            self.choose_shape()
-            return
 
-        if self.check_collision(new_pos):
-            self.set_shape()
-            self.choose_shape()
-            return
+    def collision(self, shape=None):
+        x = self.shape_pos[1]
+        y = self.shape_pos[0]
+        nx = self.next_pos[1]
+        ny = self.next_pos[0]
+        d = (ny - y, nx - x)
 
-        self.shape_pos[0] += dir[0]
-        self.shape_pos[1] += dir[1]
+        if d[0]:
+            if y + len(shape) >= 24:
+                self.save_shape()
+                return True
 
-    def check_collision(self, new_pos):
-        right = self.shape.right
-        for i in range(4):
-            for j in range(4-right):
-                if self.shape.matrix[i][j] != 0:  
-                    row_idx = new_pos[0] + i
-                    col_idx = new_pos[1] + j
-                    
-                    if self.matrix[row_idx][col_idx] != 0:
+            for i, row in enumerate(shape):
+                for j, col in enumerate(row):
+                    if col and self.in_bounds((ny+i, x+j)) and self.matrix[ny+i][x+j]:
+                        self.save_shape()
                         return True
-        return False
+
+        if d[1]:
+            if nx < 0 or nx + len(shape[0]) >= 16:
+                return True
+
+            for i, row in enumerate(shape):
+                for j, col in enumerate(row):
+                    if col and self.in_bounds((y+i, nx+j)) and self.matrix[y+i][nx+j]:
+                        return True
+
+        
+    def save_shape(self):
+        x = self.shape_pos[1]
+        y = self.shape_pos[0]
+        for i, row in enumerate(self.shape):
+            for j, col in enumerate(row):
+                if col:
+                    self.matrix[y+i][x+j] = self.shape_color
+        self.reset_shape()
+
+    def reset_shape(self):
+        self.shape = self.new_shape()
+        self.shape_color = Colors.random_color()
+        self.shape_pos = [0, 5]
+        self.next_pos = [0, 5]
 
     def move_shape(self, dir):
         new_pos = [self.shape_pos[0] + dir[0], self.shape_pos[1] + dir[1]]
@@ -152,18 +172,16 @@ class Game:
         self.shape_pos[0] += dir[0]
         self.shape_pos[1] += dir[1]
 
-    def __repr__(self):
-        result = ''
-        for row in self.matrix:
-            row_str = ''
-            for col in row:
-                if col == 0:
-                    row_str += '0 '
-                else:
-                    row_str += 'X '
-            result += row_str + '\n'
-        result += '\n'
-        return result
+    def rotate_shape(self):
+        new_shape = list(zip(*self.shape[::-1]))
+        if not self.collision(new_shape):
+            self.shape = new_shape 
+    
+    def clear_rows(self):
+        for i, row in enumerate(self.matrix):
+            if all(row):
+                self.matrix.pop(i)
+                self.matrix.insert(0, [0 for i in range(15)])
 
 
 if __name__ == '__main__':
